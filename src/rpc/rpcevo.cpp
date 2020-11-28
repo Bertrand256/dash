325376f3428e282d06bde0ebd5047a1a6b4637d7
Bertrand256 <b256@protonmail.com>
2020-11-28 20:16:54 +0100

    Removing duplicated/misplaced line.

--- src/rpc/rpcevo.cpp
+++ src/rpc/rpcevo.cpp
@@ -770,7 +770,6 @@ UniValue protx_update_registrar(const JSONRPCRequest& request)
 
     CKey keyOwner;
     if (!pwallet->GetKey(dmn->pdmnState->keyIDOwner, keyOwner)) {
-        throw std::runtime_error(strprintf("Private key for owner address %s not found in your wallet", EncodeDestination(dmn->pdmnState->keyIDOwner)));
         if(request.params.size() == 7 && request.params[6].get_str() != "") {
             keyOwner = ParsePrivKey(pwallet, request.params[6].get_str(), true);
             if(keyOwner.GetPubKey().GetID() != dmn->pdmnState->keyIDOwner)
